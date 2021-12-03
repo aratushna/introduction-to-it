1b2a54477c9e61021c0babb0f0683165780ecc84
aratushna <94103037+aratushna@users.noreply.github.com>
2021-12-03 10:45:04 +0200

    Update 1-array.cpp

--- lecture_notes/08 collections/1-array.cpp
+++ lecture_notes/08 collections/1-array.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+
+{
   int ages[] = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
 
   int first = ages[0];
@@ -8,6 +10,6 @@ int main() {
   int last = ages[length - 1];
 
 
-  printf("first: %d\n", first);
-  printf("last: %d\n", last);
+  print("first: %d\n", first);
+  print("last: %d\n", last);
 }
