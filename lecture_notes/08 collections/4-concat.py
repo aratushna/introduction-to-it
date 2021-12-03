e21df0232a9e835becc1e9f28eadcdb22d04a5d6
aratushna <94103037+aratushna@users.noreply.github.com>
2021-12-03 10:55:53 +0200

    Update 4-concat.py

--- lecture_notes/08 collections/4-concat.py
+++ lecture_notes/08 collections/4-concat.py
@@ -1,12 +1,20 @@
-# Implementation
-
 getFirstAndLast = lambda array: (array[0], array[-1])
-# concat = lambda arr1, arr2: arr1 + arr2
 
-# Usage
+def _print(ages):
+    first, last = getFirstAndLast(ages)
+    print('\n'+'ages:', ages)
+    print('first:', first, ', last:', last)
+    return
+
 
 schoolAges = [10, 12, 15, 15]
 studentAges = [17, 18, 18, 19, 20]
 ages = schoolAges + studentAges
-first, last = getFirstAndLast(ages)
-print({ 'first': first, 'last': last })
+_print(ages)
+
+# splitting the list using a filter
+schoolAges_2 = list(filter(lambda x: x < 17, ages))
+_print(schoolAges_2)
+studentAges_2 = list(filter(lambda x: x >= 17, ages))
+_print(studentAges_2)
+
